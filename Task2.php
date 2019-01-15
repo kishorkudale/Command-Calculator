963238a9d5b96de18414d2b56eb60495f933b77c
kishor.kudale@deveninfotech.com <kishor.kudale@deveninfotech.com>
2019-01-15 23:35:41 +0530

    Task 2

---
+++ Task2.php
@@ -0,0 +1,34 @@
+<?php
+class calculator
+{
+    public function __construct($argv)
+    {
+        if(count($argv)>1)
+        {
+            switch ($argv['1']) 
+            {
+                case 'add':
+                {
+                    $numbers_array = explode(',', $argv['2']??$argv['2']??0);
+                    $sum=0;
+                    foreach($numbers_array as $item)
+                    {
+                        $sum=$sum+$item;
+                    }
+                    echo $sum;                    
+                    break;
+                }
+                default: 
+                {
+                    echo 'No operation specified';
+                }
+            }    
+        }    
+        else
+        {
+            echo 'insufficient arguments passed';
+        }
+    }
+}
+$c = new calculator($argv);
+?>
